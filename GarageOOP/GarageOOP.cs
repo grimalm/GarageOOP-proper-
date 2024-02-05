1a459eaf953401d1b322fea522ccaf68281e93d0
Michael Grimaldi <mvg.grimaldi@gmail.com>
2024-02-05 20:32:12 +0000

    Add project files.

---
+++ GarageOOP/GarageOOP.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+public class garage
+{
+    //attributes
+    private double NumWheels;
+
+    List<double> Wheels = new List<double> { 2, 3, 4, 6, 8 };
+
+    ,,
+    //properties
+
+    private double _engineSize; //attribute used inside property
+    public double EngineSize //fully implemented property
+    {
+        get => _engineSize;
+        set
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentOutOfRangeException($"{value} not an acceptable engine size");
+            }
+            else
+            {
+                _engineSize = value;
+            }
+        }
+    }
+
+    public string Brand { get; private set; } //auto implemented property
+
+
+    //constructor
+    public garage(double enginesize, double numwheels, string brand)
+    {
+        EngineSize = enginesize;
+        NumWheels = numwheels;
+        Brand = brand;
+    }
+
+    //methods
+    //getter
+    public double GetNumWheels() { return NumWheels; }
+
+    public bool SetNumWheels(double edit)
+    {
+
+        foreach (double num in Wheels)
+        {
+
+            if (edit == num)
+            {
+                NumWheels = edit;
+                return true;
+            }
+
+        }
+        return false;
+
+    }
+
+    public double CalcWeight()
+    {
+        double weight = 0;
+
+        double Enginesquared = Math.Pow(EngineSize, 2);
+
+        weight = Enginesquared * GetNumWheels();
+
+        return weight;
+    }
+}
---
+++ GarageOOP/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GarageOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
